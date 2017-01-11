c9208e3972614be8df0f3e3ac8b324a9874da934
Ugis Lacis <ugis.lacis@gmail.com>
2017-01-11 12:21:34 +0100

    Modified README, intruducing the software for the poroelastic scientific contribution

--- Porous_full_bc2ifScales/3D_MSE_bc_velocity/src/3D_MSE_bc_gen_geom.py
+++ Porous_full_bc2ifScales/3D_MSE_bc_velocity/src/3D_MSE_bc_gen_geom.py
@@ -4,20 +4,20 @@
 # Ugis Lacis, ugis.lacis@gmail.com
 # Shervin Bagheri, shervin.bagheri@mech.kth.se
 # -------------------------------------------- LICENSE LGPLv3 ------------------------------------
-# This file is part of flowMSE.
+# This file is part of Porous_full_bc2ifScales.
 #
-# flowMSE is free software: you can redistribute it and/or modify
+# Porous_full_bc2ifScales is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# flowMSE is distributed in the hope that it will be useful,
+# Porous_full_bc2ifScales is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with flowMSE. If not, see <http://www.gnu.org/licenses/>.
+# along with Porous_full_bc2ifScales. If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------------------- DESCRIPTION -------------------------------------
 #
 # This software is generating the geometry definition for Gmsh mesher. It defines all necessary
